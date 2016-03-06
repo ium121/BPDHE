acbe3b9099bd3fb9150d7455351a341eed3a716f
ium121 <indrajit.psu@gmail.com>
2016-03-06 20:41:49 +0530

    Update bpdhe.cpp

--- bpdhe.cpp
+++ bpdhe.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main(int argc, char** argv[])
 {
-	cv::Mat img = cv::imread("el4.jpg");
+	cv::Mat img = cv::imread("image.jpg");
 	namedWindow("Input Image", CV_WINDOW_AUTOSIZE);
 	//namedWindow("Grayscale", CV_WINDOW_AUTOSIZE);
 	imshow("Input Image", img);
@@ -37,8 +37,6 @@ int main(int argc, char** argv[])
 		double b = 1.0f / (img.rows*img.cols * 3);
 		double x = t*b;
 		B.at<double>(k + 1) = x;
-		//cout << "Length =" << endl << "" << t1 << endl << endl;
-		//cout << "Calc = " << x << endl;
 	}
 	
 
@@ -48,14 +46,10 @@ int main(int argc, char** argv[])
 			T1.at<double>(i) = B.at<double>(j)+T1.at<double>(i);
 		}
 	}
-	//namedWindow("T1", CV_WINDOW_AUTOSIZE);
-	//imshow("T1", T1);
   Mat T2 = Mat(1, 256, CV_64F, double(0));
 	for (int i = 1; i < 256; i++){
 		T2.at<double>(i) = round((T1.at<double>(i) * 255) + 0.5);
 	}
-	//namedWindow("T2", CV_WINDOW_AUTOSIZE);
-	//imshow("T2", T2);
 
 	Mat bpdhe; img.copyTo(bpdhe);
 	for (int i = 0; i < 255; i++){
@@ -73,7 +67,6 @@ int main(int argc, char** argv[])
 
 	namedWindow("Ouptut Image", CV_WINDOW_AUTOSIZE);
 	imshow("Output Image", bpdhe);
-	imwrite("out.jpg", bpdhe);
 
 	waitKey(0);
 	destroyAllWindows();
